5b1f48462ed1128eeff2fb16710184ff7814201c
Romildo Junior <juninho_ras@hotmail.com>
2021-11-25 23:14:38 -0300

    feat: Add User routes to the express application

--- src/api/app.js
+++ src/api/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 
 const app = express();
+const userRoutes = require('./routes/User');
+
+app.use(express.json());
+
+app.use(userRoutes);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
