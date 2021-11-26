b25a22148d0ef3d1a23995e9f6d9997e3fb3dd99
Romildo Junior <juninho_ras@hotmail.com>
2021-11-25 23:17:06 -0300

    config: Change mongodb url to test the reviewer

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const MONGO_DB_URL = 'mongodb://localhost:27017/Cookmaster';
+const MONGO_DB_URL = 'mongodb://mongodb:27017/Cookmaster';
 
 mongoose.connect(MONGO_DB_URL);
 
