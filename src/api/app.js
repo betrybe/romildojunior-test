ec78eec13eeb83990e75a5c6b77ad3b0be223f6b
Romildo Junior <juninho_ras@hotmail.com>
2021-12-01 22:23:07 -0300

    feat: Add Auth routes to the express application

--- src/api/app.js
+++ src/api/app.js
@@ -3,8 +3,10 @@ const express = require('express');
 const app = express();
 const userRoutes = require('./routes/User');
 const recipeRoutes = require('./routes/Recipe');
+const authRoutes = require('./routes/Auth');
 app.use(userRoutes);
 app.use(recipeRoutes);
+app.use(authRoutes);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
