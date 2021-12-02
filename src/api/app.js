437344f50c11f345589a6a6d7d69e6f07c5b7ef5
Romildo Junior <juninho_ras@hotmail.com>
2021-12-01 22:21:13 -0300

    feat: Add Recipe routes to the express application

--- src/api/app.js
+++ src/api/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const app = express();
 const userRoutes = require('./routes/User');
+const recipeRoutes = require('./routes/Recipe');
 app.use(userRoutes);
+app.use(recipeRoutes);
 
 // Não remover esse end-point, ele é necessário para o avaliador
 app.get('/', (request, response) => {
