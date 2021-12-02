c5ad826b770b36b1f9aa1b50f0f27649e6fdddd0
Romildo Junior <juninho_ras@hotmail.com>
2021-12-01 22:37:35 -0300

    config: Define MONGO_DB_URL to use the code reviewer

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const config = {
   JWT_TOKEN: '@TRYBE_TOKEN',
-  MONGO_DB_URL: 'mongodb://localhost:27017/Cookmaster',
+  MONGO_DB_URL: 'mongodb://mongodb:27017/Cookmaster',
   SESSION_TOKEN: '@TRYBE_SESSION',
 };
 
